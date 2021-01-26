4be94604d6e5979e87768aa5db561e8be1f4234f
jw <jeb.williams8@gmail.com>
2021-01-26 18:58:19 +1000

    change "crud" to "flight" in flight controller

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,23 +1,24 @@
 import asyncHandler from 'express-async-handler'
+import flights from '../data/flights.js'
 import Flight from '../models/flightModel.js'
 
 
 // @desc    Create new flight
-// @route   POST /crud
+// @route   POST /flights
 // @access  Private
 
 const addFlights = asyncHandler(async (req, res) => {
     console.log(req.body)
 
-    const flight = new Crud(req.body)
+    const flight = new Flight(req.body)
     
-    const createdCrud = await crud.save()
+    const createdFlight = await flight.save()
 
-    res.status(201).json(createdCrud)
+    res.status(201).json(createdFlight)
 })
 
 
-// @desc Fetch all cruds
+// @desc Fetch all flights
 // @route GET /flight
 // @access Public
 const getFlights = asyncHandler(async (req, res) => {
